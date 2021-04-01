c0e7c64f861f39992868df99248b9e198172239a
Kochiyama <marcelokochiyama2210@gmail.com>
2021-04-01 17:29:05 -0400

    Create profile component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,65 @@
 import React from 'react';
+import styled from 'styled-components';
+import BasicInfo from '../components/BasicInfo';
+import InfoGrid from '../components/InfoGrid';
 
-const Profile = ({
-  avatar,
-  backgroundImg,
-  fullname,
-  username,
-  email,
-  address,
-  bio,
-  instagramLink,
-  linkedinLink,
-}) => {
-  return <h1>Profile Page</h1>;
+const Page = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BackgroundImg = styled.img`
+  width: 100vw;
+  height: 30vh;
+  object-fit: cover;
+  z-index: -999;
+  position: absolute;
+  top: 0;
+`;
+
+const ProfileContainer = styled.main`
+  max-width: 500px;
+`;
+
+const Profile = ({ userData }) => {
+  const {
+    avatar,
+    backgroundImg,
+    fullname,
+    username,
+    email,
+    bio,
+    instagramLink,
+    linkedinLink,
+    techs,
+  } = userData;
+
+  return (
+    <Page>
+      <BackgroundImg src={backgroundImg} alt='User Background' />
+
+      <ProfileContainer>
+        <BasicInfo
+          avatar={avatar}
+          fullname={fullname}
+          username={username}
+          email={email}
+        />
+
+        <InfoGrid
+          fullname={fullname}
+          bio={bio}
+          instagramLink={instagramLink}
+          linkedinLink={linkedinLink}
+          techs={techs}
+        />
+      </ProfileContainer>
+    </Page>
+  );
 };
 
 export default Profile;
