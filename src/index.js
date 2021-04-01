e798a307f364ad2887a361cd0a63b574debd9b76
Kochiyama <marcelokochiyama2210@gmail.com>
2021-04-01 17:28:16 -0400

    update background image and replace address by an array of techs

--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ import './index.css';
 const userData = {
   avatar: 'https://avatars.githubusercontent.com/u/26096036?v=4',
   backgroundImg:
-    'https://images.unsplash.com/photo-1593642532454-e138e28a63f4?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+    'https://images.unsplash.com/photo-1514205447349-072b4f7d7cb8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1566&q=80',
   fullname: 'Marcelo Haruo Kochiyama',
   username: 'kochiyama',
   email: 'marcelokochiyama@email.com',
-  address: 'Github street, 2021',
   bio:
     'Undergraduate in Software Engineering at UNIGRAN. Lover of programming, music and games. Trying to build a solid learning path.',
   instagramLink: 'https://www.instagram.com/mhkochiyama/',
   linkedinLink: 'https://www.linkedin.com/in/marcelo-kochiyama/',
+  techs: ['React', 'NodeJS', 'Rust'],
 };
 
 ReactDOM.render(
